0d9a88cc33a19ed26fad127a52932389c5aed3b2
Alejandro <alejandrocs2115@gmail.com>
2019-03-13 22:27:40 -0500

    Primer commit B para subir a server

---
+++ app/Http/Controllers/TypePersonController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TypePerson;
+use Illuminate\Http\Request;
+
+class TypePersonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TypePerson  $typePerson
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TypePerson $typePerson)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TypePerson  $typePerson
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TypePerson $typePerson)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TypePerson  $typePerson
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TypePerson $typePerson)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});*/
+
+Route::apiResource('typepeople','TypePersonController');
+Route::apiResource('addresses','AddressController');
+Route::apiResource('people','PersonController');
+Route::apiResource('users','UserController');
